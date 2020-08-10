4586282a89eca60550f9880c51074540a140479f
Immanuel Van Meirhaeghe <Immanuel.VanMeirhaeghe@sii.be>
2020-08-10 03:06:36 +0200

    Add project files.

---
+++ ModConstructions.cs
@@ -0,0 +1,275 @@
+﻿using Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ModConstructions
+{
+    class ModConstructions : MonoBehaviour
+    {
+        private static ModConstructions s_Instance;
+
+        private bool showUI = false;
+
+        private static ItemsManager itemsManager;
+
+        private static Player player;
+
+        private static HUDManager hUDManager;
+
+        public bool IsModConstructionsActive = false;
+
+        public static bool HasUnlockedConstructions = false;
+
+        public static bool HasUnlockedShelters = false;
+
+        public bool IsOptionInstantFinishConstructionsActive;
+
+        /// <summary>
+        /// ModAPI required security check to enable this mod feature.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsLocalOrHost => ReplTools.AmIMaster() || !ReplTools.IsCoopEnabled();
+
+        public ModConstructions()
+        {
+            IsModConstructionsActive = true;
+            s_Instance = this;
+        }
+
+        public static ModConstructions Get()
+        {
+            return s_Instance;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Home))
+            {
+                if (!showUI)
+                {
+                    hUDManager = HUDManager.Get();
+
+                    itemsManager = ItemsManager.Get();
+
+                    player = Player.Get();
+
+                    EnableCursor(true);
+                }
+                // toggle menu
+                showUI = !showUI;
+                if (!showUI)
+                {
+                    EnableCursor(false);
+                }
+            }
+            if (!IsOptionInstantFinishConstructionsActive && !IsLocalOrHost && IsModConstructionsActive && Input.GetKeyDown(KeyCode.F8))
+            {
+                ShowHUDBigInfo("Feature disabled in multiplayer!", "Mod Constructions Info", HUDInfoLogTextureType.Count.ToString());
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (showUI)
+            {
+                InitData();
+                InitModUI();
+            }
+        }
+
+        private static void InitData()
+        {
+            hUDManager = HUDManager.Get();
+
+            itemsManager = ItemsManager.Get();
+
+            player = Player.Get();
+
+            InitSkinUI();
+        }
+
+        private static void InitSkinUI()
+        {
+            GUI.skin = ModAPI.Interface.Skin;
+        }
+
+        private void InitModUI()
+        {
+            GUI.Box(new Rect(10f, 10f, 550f, 100f), "ModConstructions UI", GUI.skin.window);
+
+            GUI.Label(new Rect(30f, 30f, 300f, 20f), "Click to unlock all constructions", GUI.skin.label);
+            if (GUI.Button(new Rect(350f, 30f, 150f, 20f), "Unlock constructions", GUI.skin.button))
+            {
+                OnClickUnlockConstructionsButton();
+                showUI = false;
+                EnableCursor(false);
+            }
+
+            GUI.Label(new Rect(30f, 50f, 300f, 20f), "Click to unlock all shelters and beds", GUI.skin.label);
+            if (GUI.Button(new Rect(350f, 50f, 150f, 20f), "Unlock shelters", GUI.skin.button))
+            {
+                OnClickUnlockSheltersButton();
+                showUI = false;
+                EnableCursor(false);
+            }
+
+            GUI.Label(new Rect(30f, 70f, 300f, 20f), "Use F8 to instantly finish constructions", GUI.skin.label);
+            IsOptionInstantFinishConstructionsActive = GUI.Toggle(new Rect(350f, 70f, 20f, 20f), IsOptionInstantFinishConstructionsActive, "");
+        }
+
+        public bool UseOptionF8
+        {
+            get
+            {
+                return IsOptionInstantFinishConstructionsActive;
+            }
+        }
+
+        public static void OnClickUnlockConstructionsButton()
+        {
+            try
+            {
+                UnlockAllConstructions();
+            }
+            catch (Exception exc)
+            {
+                ModAPI.Log.Write($"[{nameof(ModConstructions)}.{nameof(ModConstructions)}:{nameof(OnClickUnlockConstructionsButton)}] throws exception: {exc.Message}");
+            }
+        }
+
+        public static void OnClickUnlockSheltersButton()
+        {
+            try
+            {
+                UnlockAllShelters();
+            }
+            catch (Exception exc)
+            {
+                ModAPI.Log.Write($"[{nameof(ModConstructions)}.{nameof(ModConstructions)}:{nameof(OnClickUnlockSheltersButton)}] throws exception: {exc.Message}");
+            }
+        }
+
+        public static void UnlockAllConstructions()
+        {
+            try
+            {
+                if (!HasUnlockedConstructions)
+                {
+                    List<ItemInfo> list = itemsManager.GetAllInfosOfType(ItemType.Construction);
+                    foreach (ItemInfo constructionItemInfo in list)
+                    {
+                        if (constructionItemInfo.m_ID != ItemID.Ayuhasca_Cauldron_Dream01_Rack_New
+                             || constructionItemInfo.m_ID != ItemID.Ayuhasca_Cauldron_Dream02_Rack_New
+                             || constructionItemInfo.m_ID != ItemID.Ayuhasca_Cauldron_Dream03_Rack_New
+                             || constructionItemInfo.m_ID != ItemID.Ayuhasca_Cauldron_Dream04_Rack_New)
+                        {
+                            itemsManager.UnlockItemInNotepad(constructionItemInfo.m_ID);
+                            itemsManager.UnlockItemInfo(constructionItemInfo.m_ID.ToString());
+                            ShowHUDInfoLog(constructionItemInfo.m_ID.ToString(), "HUD_InfoLog_NewEntry");
+                        }
+                    }
+                    HasUnlockedConstructions = true;
+                }
+                else
+                {
+                    ShowHUDBigInfo("All constructions unlocked", "Mod Constructions Info", HUDInfoLogTextureType.Count.ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+                ModAPI.Log.Write($"[{nameof(ModConstructions)}.{nameof(ModConstructions)}:{nameof(UnlockAllConstructions)}] throws exception: {exc.Message}");
+            }
+        }
+
+        public static void UnlockAllShelters()
+        {
+            try
+            {
+                if (!HasUnlockedShelters)
+                {
+                    List<ItemInfo> list = itemsManager.GetAllInfos().Values.Where(info => info.IsShelter()).ToList();
+
+                    if (!list.Contains(itemsManager.GetInfo(ItemID.Hut_Shelter)))
+                    {
+                        list.Add(itemsManager.GetInfo(ItemID.Hut_Shelter));
+                    }
+
+                    if (!list.Contains(itemsManager.GetInfo(ItemID.Medium_Bamboo_Shelter)))
+                    {
+                        list.Add(itemsManager.GetInfo(ItemID.Medium_Bamboo_Shelter));
+                    }
+
+                    if (!list.Contains(itemsManager.GetInfo(ItemID.Medium_Shelter)))
+                    {
+                        list.Add(itemsManager.GetInfo(ItemID.Medium_Shelter));
+                    }
+
+                    if (!list.Contains(itemsManager.GetInfo(ItemID.Small_Bamboo_Shelter)))
+                    {
+                        list.Add(itemsManager.GetInfo(ItemID.Small_Bamboo_Shelter));
+                    }
+
+                    foreach (ItemInfo shelterItemInfo in list)
+                    {
+                        itemsManager.UnlockItemInNotepad(shelterItemInfo.m_ID);
+                        itemsManager.UnlockItemInfo(shelterItemInfo.m_ID.ToString());
+                        ShowHUDInfoLog(shelterItemInfo.m_ID.ToString(), "HUD_InfoLog_NewEntry");
+                    }
+                    HasUnlockedShelters = true;
+                }
+                else
+                {
+                    ShowHUDBigInfo("All shelters unlocked", "Mod Constructions Info", HUDInfoLogTextureType.Count.ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+                ModAPI.Log.Write($"[{nameof(ModConstructions)}.{nameof(ModConstructions)}:{nameof(UnlockAllShelters)}] throws exception: {exc.Message}");
+            }
+        }
+
+        public static void ShowHUDInfoLog(string itemID, string localizedTextKey)
+        {
+            Localization localization = GreenHellGame.Instance.GetLocalization();
+            ((HUDMessages)hUDManager.GetHUD(typeof(HUDMessages))).AddMessage(localization.Get(localizedTextKey) + "  " + localization.Get(itemID));
+        }
+
+        public static void ShowHUDBigInfo(string text, string header, string textureName)
+        {
+            HUDManager hUDManager = HUDManager.Get();
+
+            HUDBigInfo hudBigInfo = (HUDBigInfo)hUDManager.GetHUD(typeof(HUDBigInfo));
+            HUDBigInfoData hudBigInfoData = new HUDBigInfoData
+            {
+                m_Header = header,
+                m_Text = text,
+                m_TextureName = textureName,
+                m_ShowTime = Time.time
+            };
+            hudBigInfo.AddInfo(hudBigInfoData);
+            hudBigInfo.Show(true);
+        }
+
+        private static void EnableCursor(bool enabled = false)
+        {
+            CursorManager.Get().ShowCursor(enabled, false);
+            player = Player.Get();
+
+            if (enabled)
+            {
+                player.BlockMoves();
+                player.BlockRotation();
+                player.BlockInspection();
+            }
+            else
+            {
+                player.UnblockMoves();
+                player.UnblockRotation();
+                player.UnblockInspection();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ModConstructions.Properties")]
+[assembly: AssemblyDescription("ModConstructions is a mod for Green Hel, which allows a player to unlock all constructions and unlock all shelter types. It also gives the player the possibility to instantly finish any ongoing building. Press Home to activate the mod. If enabled, Press F8 to finish all constructions (only in single player mode).")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModConstructions.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ac86231-af2d-48ad-bed0-9f2feb291c9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
